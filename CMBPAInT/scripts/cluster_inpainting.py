f2bf07c8b6435a4ca9eb41196b36f0b71ba0c098
Christian Gimeno <122972528+ChristianGim@users.noreply.github.com>
2026-04-22 10:03:38 +0200

    Update cluster_inpainting.py

--- CMBPAInT/scripts/cluster_inpainting.py
+++ CMBPAInT/scripts/cluster_inpainting.py
@@ -315,7 +315,7 @@ if __name__ == '__main__':
     import configparser
     import healpy as hp
     from tqdm import tqdm
-    from Tools import inpainting_tools
+    from CMBPAInT.Tools import inpainting_tools
 
     def initial():
         """Function to read input arguments from command line and to offer help to user."""
